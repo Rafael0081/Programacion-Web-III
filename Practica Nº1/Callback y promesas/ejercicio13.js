995c3a6a7fba575eaf1eae9d90640797da23caaa
Rafael0081 <smurfjoy1@gmail.com>
2026-03-30 10:08:22 -0400

    Refactor callback functions to use async/await

--- Practica Nº1/Callback y promesas/ejercicio13.js
+++ Practica Nº1/Callback y promesas/ejercicio13.js
@@ -1,27 +1,28 @@
 //antes de usar promesas, el código con callbacks se veia asi:
-const fs = require('fs');
-function procesarArchivoCallback() {
-    fs.readFile('entrada.txt', 'utf8', (err, data) => {
-        if (err) return console.error("Error al leer:", err);
-        
-        const procesado = data.toUpperCase();
-        
-        fs.writeFile('salida.txt', procesado, (err) => {
-            if (err) return console.error("Error al escribir:", err);
-            console.log('Archivo procesado y guardado con éxito.');
-        });
-    });
+function obtenerComentariosDelUsuario() {
+  fetchUser(1)
+    .then(user => {
+      fetchPosts(user.id)
+        .then(posts => {
+          fetchComments(posts[0].id)
+            .then(comments => {
+              console.log("Comentarios:", comments);
+            })
+            .catch(err => console.error("Error en comentarios", err));
+        })
+        .catch(err => console.error("Error en posts", err));
+    })
+    .catch(err => console.error("Error en usuario", err));
 }
 //con sync/await y promesas, el codigo se ve asi:
-
-const fs = require('fs').promises;
-async function procesarArchivoAsync() {
-    try {
-        const data = await fs.readFile('entrada.txt', 'utf8');
-        const procesado = data.toUpperCase();
-        await fs.writeFile('salida.txt', procesado);
-        console.log('Archivo procesado y guardado con éxito.');
-    } catch (err) {
-        console.error("Ocurrió un error en el proceso:", err);
-    }
+async function obtenerComentariosDelUsuario() {
+  try {
+    const user = await fetchUser(1);
+    const posts = await fetchPosts(user.id);
+    const comments = await fetchComments(posts[0].id);
+    
+    console.log("Comentarios:", comments);
+  } catch (error) {
+    console.error("Ocurrió un error:", error);
+  }
 }
